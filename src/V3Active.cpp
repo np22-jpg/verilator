03f5780f49038015a98769d75706078c63d10195
Nolan Poe <npgo22@gmail.com>
2024-04-07 17:00:00 -0700

    y

--- src/V3Active.cpp
+++ src/V3Active.cpp
@@ -645,7 +645,7 @@ public:
 // Active class functions
 
 void V3Active::activeAll(AstNetlist* nodep) {
-    UINFO(2, __FUNCTION__ << ": " << endl);
+    UINFO(2, __FUNCTION__ << ": " << '\n');
     { ActiveVisitor{nodep}; }  // Destruct before checking
     V3Global::dumpCheckGlobalTree("active", 0, dumpTreeEitherLevel() >= 3);
 }
